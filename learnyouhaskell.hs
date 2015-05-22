c6d354893fa9f56d768383c3b74792b6b5cf1d49
Paul Kruger <paul@miamiphp.org>
2015-05-22 14:25:13 -0400

    adding notes

--- learnyouhaskell.hs
+++ learnyouhaskell.hs
@@ -1,3 +1,4 @@
+-- starting-out
 doubleMe x = x + x
 doubleUs x y = doubleMe x + doubleMe y
 
@@ -6,9 +7,23 @@ doubleSmallNumber x = if x > 100
                         else x*2
 doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
 
-circumference r = 2 * pi * r
+conanO'Brien = "It's a-me, Conan O'Brien!"
+
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
 removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
 
+triangles = [ (a,b,c) | c <- [1..10], b <- [1..10], a <- [1..10] ]
+
+rightTriangles = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2]
+
+rightTriangles' = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a+b+c == 24]
+
+circumference r = 2 * pi * r
+
+-- types-and-typeclasses
+
+-- syntax-in-functions
 sayMe :: (Integral a) => a -> String
 sayMe 1 = "One!"
 sayMe 2 = "Two!"
