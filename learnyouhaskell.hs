72d6daa758ee57dcf62a8b16497f36269da53b0f
Paul Kruger <paul@miamiphp.org>
2015-05-22 14:30:39 -0400

    fixing errors

--- learnyouhaskell.hs
+++ learnyouhaskell.hs
@@ -11,7 +11,6 @@ conanO'Brien = "It's a-me, Conan O'Brien!"
 
 boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
 
-removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
 
 triangles = [ (a,b,c) | c <- [1..10], b <- [1..10], a <- [1..10] ]
 
@@ -19,9 +18,16 @@ rightTriangles = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 =
 
 rightTriangles' = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a+b+c == 24]
 
-circumference r = 2 * pi * r
-
 -- types-and-typeclasses
+
+removeNonUppercase :: [Char] -> [Char]
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
+
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x + y + z
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
 
 -- syntax-in-functions
 sayMe :: (Integral a) => a -> String
